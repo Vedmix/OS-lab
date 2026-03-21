c2ced1f7f243429e78d4f68ef3f2d7ef10c6bc72
tiliberkoo <vishnyakovdanya@mail.ru>
2026-03-21 15:27:55 +0300

    added forth function almost

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -20,6 +20,7 @@ int main()
         std::cout<<"1. Output list of disks (GetLogicalDriveStrings)\n";
         std::cout<<"2. Output list of disks (GetLogicalDrives)\n";
         std::cout<<"3. Driver Type (GetDriverType)\n";
+        std::cout<<"4. Driver info (GetVolumeInformation)\n";
         std::cout<<"0. Exit\n\n";
         std::cin>>menu;
         std::cout<<"\n";
@@ -98,6 +99,55 @@ int main()
                     }
                     break;
                 }
+            }
+            break;
+            case 4:
+            {
+                if(listofdrivers.empty()){
+                    std::cout<<"Please check your drives first (Option 1-2 in menu)"<<std::endl; 
+                    break;
+                }
+                CHAR VolumeNameBuffer[MAX_SIZE];
+                CHAR FileSystemNameBuffer[MAX_SIZE];
+                DWORD componentlenght,flags;
+                LPDWORD lpMaximumComponentLength = &componentlenght;
+                DWORD serial;
+                LPSTR lpVolumeNameBuffer = VolumeNameBuffer;
+                LPDWORD lpVolumeSerialNumber = &serial;
+                LPDWORD lpFileSystemFlags = &flags;
+                LPSTR lpFileSystemNameBuffer = FileSystemNameBuffer;
+                std::cout<<"Please select a disk number"<<std::endl;
+                std::cin>>id;
+                BOOL info = GetVolumeInformationA(listofdrivers[id].c_str(),lpVolumeNameBuffer,MAX_SIZE,lpVolumeSerialNumber,lpMaximumComponentLength,lpFileSystemFlags,lpFileSystemNameBuffer,MAX_SIZE);
+                if(info){
+                    std::cout<<"Tom: " << VolumeNameBuffer<< std::endl;
+                    std::cout<<"Serial number: " << serial << std::endl;
+                    std::cout<<"File system: "<< FileSystemNameBuffer<< std::endl;
+                    std::cout<<"Max len of name: " << *lpMaximumComponentLength<< std::endl;
+                    std::cout<<"File system flags: " << flags<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_PRESERVED_NAMES) std::cout<<"The specified volume supports preserved case of file names when it places a name on disk."<<std::endl;
+                    if(flags & FILE_UNICODE_ON_DISK) std::cout<<"The specified volume supports Unicode in file names as they appear on disk."<<std::endl;
+                    if(flags & FILE_PERSISTENT_ACLS) std::cout<<"The specified volume preserves and enforces access control lists (ACL). For example, the NTFS file system preserves and enforces ACLs, and the FAT file system does not."<<std::endl;
+                    if(flags & FILE_FILE_COMPRESSION) std::cout<<"The specified volume supports file-based compression."<<std::endl;
+                    if(flags & FILE_VOLUME_QUOTAS) std::cout<<"The specified volume supports disk quotas."<<std::endl;
+                    if(flags & FILE_SUPPORTS_SPARSE_FILES) std::cout<<"The specified volume supports sparse files."<<std::endl;
+                    if(flags & FILE_SUPPORTS_REPARSE_POINTS) std::cout<<"The specified volume supports reparse points."<<std::endl;
+                    if(flags & FILE_SUPPORTS_REMOTE_STORAGE) std::cout<<"The file system supports remote storage."<<std::endl;
+                    if(flags & FILE_RETURNS_CLEANUP_RESULT_INFO) std::cout<<"On a successful cleanup operation, the file system returns information that describes additional actions taken during cleanup, such as deleting the file."<<std::endl;
+                    if(flags & FILE_SUPPORTS_POSIX_UNLINK_RENAME) std::cout<<"The file system supports POSIX-style delete and rename operations."<<std::endl;
+                    if(flags & FILE_VOLUME_IS_COMPRESSED) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_SUPPORTS_OBJECT_IDS) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                    if(flags & FILE_CASE_SENSITIVE_SEARCH) std::cout<<"The specified volume supports case-sensitive file names."<<std::endl;
+                }
+                else{
+                    std::cout<<"Error"<<std::endl;
+                }
+
             }
             break;
             case 0:
